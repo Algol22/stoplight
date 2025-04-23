9fdbbc0a2cd8d01b1e3011154279380ad40e12af
Algol <andriyslb@gmail.com>
2025-04-23 23:55:45 +0300

    Added 7 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI3OTI4Ng/spectral.js?branch_slug=main&token=a8d5be42-808b-4578-92f7-e7837ee91b72";
+export default { extends: ruleset };
